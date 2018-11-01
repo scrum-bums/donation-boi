3cc895d1f37365e94a40cda44f11f0d3de2b345a
Evan Strat <evan10s@users.noreply.github.com>
2018-11-01 04:04:50 -0400

    Update to handle minor changes to Item class
    Update Item import and handle new Item constructor

--- app/src/main/java/com/scrumbums/donationboi/controllers/ItemView.java
+++ app/src/main/java/com/scrumbums/donationboi/controllers/ItemView.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.scrumbums.donationboi.R;
-import com.scrumbums.donationboi.model.Item;
+import com.scrumbums.donationboi.model.entities.Item;
 import com.scrumbums.donationboi.model.entities.Store;
 import com.scrumbums.donationboi.model.util.DatabaseAbstraction;
 
@@ -34,7 +34,7 @@ public class ItemView extends AppCompatActivity {
         //TODO: show error if invalid store/item
 
         if (item == null) {
-            item = new Item("Item not found");
+            item = new Item("Item not found", store);
         }
 
         nameView = findViewById(R.id.item_name);
