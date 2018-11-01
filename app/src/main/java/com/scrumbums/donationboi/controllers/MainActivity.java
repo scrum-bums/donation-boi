9752cbdb135d15d6d34a1e1e66df4ef49a1f0a1a
Evan Strat <evan10s@users.noreply.github.com>
2018-11-01 04:05:40 -0400

    Return to Welcome screen after registration is complete

--- app/src/main/java/com/scrumbums/donationboi/controllers/MainActivity.java
+++ app/src/main/java/com/scrumbums/donationboi/controllers/MainActivity.java
@@ -2,9 +2,9 @@ package com.scrumbums.donationboi.controllers;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
@@ -45,7 +45,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
-                finish();
             }
         });
     }
