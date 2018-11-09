228caee276b0190adf6d2b083b3a7b868dca5d2d
Gibran Essa <gibran.essa@gmail.com>
2018-11-08 22:24:43 -0500

    fixed issues for mega important code-review

--- app/src/main/java/com/scrumbums/donationboi/controllers/StoreListActivity.java
+++ app/src/main/java/com/scrumbums/donationboi/controllers/StoreListActivity.java
@@ -14,10 +14,6 @@ import com.scrumbums.donationboi.model.util.DatabaseAbstraction;
 
 public class StoreListActivity extends AppCompatActivity {
     private Store[] stores;
-    private ListView listView;
-    private Button logout;
-    private Button searchBtn;
-    private Button mapBtn;
     private ArrayAdapter adapter;
     final String TAG = "DONATION-BOI/StoreList";
 
@@ -26,40 +22,33 @@ public class StoreListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.store_list);
 
-        listView = findViewById(R.id.mobile_list);
+        ListView listView = findViewById(R.id.mobile_list);
         stores = DatabaseAbstraction.getStoresArrayList();
         adapter = new ArrayAdapter<>(this, R.layout.csv_element_view, stores);
         listView.setAdapter(adapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?>adapter, View v, int position, long id) {
-                Store e = stores[position];
-                Intent intent = new Intent(StoreListActivity.this, StoreViewActivity.class);
-                intent.putExtra("storeId", e.getStoreId());
-                Bundle bundle = intent.getExtras();
-                startActivity(intent);
-            }
+        listView.setOnItemClickListener((adapter, v, position, id) -> {
+            Store e = stores[position];
+            Intent intent = new Intent(StoreListActivity.this, StoreViewActivity.class);
+            intent.putExtra("storeId", e.getStoreId());
+            startActivity(intent);
         });
 
-        logout = findViewById(R.id.logout);
+        Button logout = findViewById(R.id.logout);
         logout.setOnClickListener(v -> {
             DatabaseAbstraction.logout(getApplicationContext());
             startActivity(new Intent(StoreListActivity.this, MainActivity.class));
             finish();
         });
 
-        searchBtn = findViewById(R.id.search_items);
-        searchBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent addItemIntent = new Intent(StoreListActivity.this, ItemSearchActivity.class);
-                addItemIntent.putExtra("storeId", 0);
-                startActivity(addItemIntent);
-            }
+        Button searchBtn = findViewById(R.id.search_items);
+        searchBtn.setOnClickListener(v -> {
+            Intent addItemIntent = new Intent(StoreListActivity.this, ItemSearchActivity.class);
+            addItemIntent.putExtra("storeId", 0);
+            startActivity(addItemIntent);
         });
 
-        mapBtn = findViewById(R.id.goto_map);
+        Button mapBtn = findViewById(R.id.goto_map);
         mapBtn.setOnClickListener((v) -> {
             Intent mapIntent = new Intent(StoreListActivity.this, MapViewActivity.class);
             startActivity(mapIntent);
