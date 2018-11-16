0ebce9c089083ded48eee8978f6a5c1be69cbca9
Evan Strat <evan10s@users.noreply.github.com>
2018-11-15 23:43:12 -0500

    MOAR lint error fixes

--- app/src/main/java/com/scrumbums/donationboi/controllers/AddItemForm.java
+++ app/src/main/java/com/scrumbums/donationboi/controllers/AddItemForm.java
@@ -35,7 +35,7 @@ public class AddItemForm extends AppCompatActivity {
 
         nameBox = findViewById(R.id.name_box);
         priceBox = findViewById(R.id.price_box);
-        descriptionBox = findViewById(R.id.descrip_box);
+        descriptionBox = findViewById(R.id.description_box);
         typeBox = findViewById(R.id.type_box);
 
         categorySpinner = findViewById(R.id.category_spinner);
