b143c23b353091b12456d4b9ae543785bf64e7f9
cam3new <camcam.newman@gmail.com>
2018-11-16 12:50:59 -0500

    changed private getCategory to public getCategory and added a junit for that method

--- app/src/test/java/com/scrumbums/donationboi/CategoryEqualsTest.java
+++ app/src/test/java/com/scrumbums/donationboi/CategoryEqualsTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class junit {
+public class CategoryEqualsTest {
     @Test
     public void testGetCategory() {
         assertEquals(Categories.ELECTRONICS, Categories.getCategory("Electronics"));
